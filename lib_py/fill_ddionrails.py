74926cf4697dc7cf503b7413bae9d98166366d8c
hansendx <DominiqueHansen@gmx.net>
2020-07-21 10:31:11 +0200

    Unify logical_variables and questions_variables
    metadata/logical_variables.csv and ddionrails/questions_variables.csv
    were basically identical in their content.
    
    metadata/logical_variables.csv was renamed to questions_variables.csv
    which carries more meaning.
    ddionrails/questions_variables.csv was replaced with a symlink to
    metadata/logical_variables.csv.

--- lib_py/fill_ddionrails.py
+++ lib_py/fill_ddionrails.py
@@ -13,46 +13,7 @@ STUDY = "twinlife"
 VERSION = "v3-0-0"
 
 
-def questions_variables():
-
-    questions_variables = pd.read_csv("metadata/logical_variables.csv")
-    questions_variables.rename(
-        columns={
-            "study": "study_name",
-            "dataset": "dataset_name",
-            "variable": "variable_name",
-            "questionnaire": "instrument_name",
-            "question": "question_name",
-        },
-        inplace=True,
-    )
-    questions_variables = questions_variables[
-        [
-            "study_name",
-            "dataset_name",
-            "variable_name",
-            "instrument_name",
-            "question_name",
-        ]
-    ]
-
-    questions_variables.dropna(axis=0, how="any", inplace=True)
-    questions_variables.drop_duplicates(inplace=True)
-    questions_variables.to_csv("ddionrails/questions_variables.csv", index=False)
-
-
 def main():
-    # simple copy operations
-    shutil.copy("metadata/study.md", "ddionrails/study.md")
-    shutil.copy("metadata/analysis_units.csv", "ddionrails/analysis_units.csv")
-    shutil.copy(
-        "metadata/conceptual_datasets.csv", "ddionrails/conceptual_datasets.csv"
-    )
-    shutil.copy("metadata/periods.csv", "ddionrails/periods.csv")
-    shutil.copy("metadata/datasets.csv", "ddionrails/datasets.csv")
-
-    # custom operation
-    questions_variables()
 
     # operations from ddi.py
     dor1.variables()
